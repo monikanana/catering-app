ec9834f637b82700fe01d33f5567a7a97b0f19d1
Tay0108 <kjkosecki@gmail.com>
2019-06-11 23:04:24 +0200

    delivery app init

--- frontend/delivery/src/App.js
+++ frontend/delivery/src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as orders from './orders.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {List} from 'antd';
+import 'antd/dist/antd.css';
+
+class App extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            orders: []
+        };
+        this.getOrders = this.getOrders.bind(this);
+        this.completeOrder = this.completeOrder.bind(this);
+        this.cancelOrder = this.cancelOrder.bind(this);
+    }
+
+    getOrders() {
+        fetch("http://localhost:8080/catering_app_war_exploded/customers/orders")
+            .then(orders => orders.json())
+            .then(orders => {
+                this.setState({orders: orders});
+            })
+    }
+
+    componentDidMount() {
+        this.getOrders();
+        setInterval(() => this.getOrders(), 100000);
+    }
+
+    completeOrder(id) {
+
+    }
+
+    cancelOrder(id) {
+
+    }
+
+
+    render() {
+        const {orders} = this.state;
+
+        return (
+            <div className="App">
+                <div className="orders-list">
+                <h1>Zamówienia do dostarczenia:</h1>
+
+                {orders.map(order => {
+                    return (
+                        <div className="order">
+                            <h2>Zamówienie nr {order.id}</h2>
+                            <List
+                                className="orders-list"
+                                itemLayout="horizontal"
+                                dataSource={order.meals}
+                                renderItem={meal => (
+                                    <List.Item actions={[<a>anuluj</a>, <a>dostarcz</a>]}>
+                                        <List.Item.Meta
+                                            title={meal.name}
+                                            description="adres odbioru, adres dostawy"
+                                        />
+                                        <div className="order-date">{order.date}</div>
+                                    </List.Item> )}
+                            />
+                        </div>
+                    );
+                })
+                }
+                </div>
+            </div>
+          );
+    }
 }
 
 export default App;
+
+
+
+
+
+
+
