a7040b6599a10764930507f037a4f112c3617c76
Tay0108 <kjkosecki@gmail.com>
2019-06-13 20:37:09 +0200

    progress

--- frontend/dummy-restaurant/src/App.js
+++ frontend/dummy-restaurant/src/App.js
@@ -1,33 +1,51 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
 import './App.css';
+import Restaurant from './Restaurant/Restaurant';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      restaurants: []
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            restaurants: null
+        };
+    }
 
-  getRestaurants() {
-    fetch("http://localhost:8080/catering_app_war_exploded/restaurants")
-        .then(restaurants => {
-          this.setState({});
-        });
-  }
+    getRestaurants() {
+        fetch("http://localhost:8080/catering_app_war_exploded/restaurants")
+            .then(response => response.json())
+            .then(restaurants => {
+                this.setState({restaurants: restaurants});
+            });
+    }
 
-  componentDidMount() {
-    this.getRestaurants();
-  }
+    componentDidMount() {
+        this.getRestaurants();
+    }
 
-  render() {
-    return (
-        <div className="App">
+    render() {
 
-        </div>
-    );
-  }
+        const {restaurants} = this.state;
+
+        return (
+            <div className="App">
+                <BrowserRouter>
+                    <h1>Podgląd restauracji zewnętrznych:</h1>
+                    {restaurants && <ul className="restaurants-list">
+                        {
+                            restaurants.map(restaurant => (
+                                <li key={restaurant.id}>
+                                    <NavLink to={`/restaurant/${restaurant.id}`}>Restauracja {restaurant.name}</NavLink>
+                                </li>
+                            ))
+                        }
+                    </ul>}
+                    <Route exact path="/restaurant/:restaurantId" component={Restaurant}/>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
--- frontend/dummy-restaurant/src/Restaurant/Restaurant.js
+++ frontend/dummy-restaurant/src/Restaurant/Restaurant.js
@@ -5,14 +5,59 @@ class Restaurant extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            restaurantId: props.match.params.restaurantId
+        };
+        this.getRestaurantInfo = this.getRestaurantInfo.bind(this);
+        this.getMeals = this.getMeals.bind(this);
+    }
+
+    getRestaurantInfo() {
+        fetch("http://localhost:8080/catering_app_war_exploded/restaurants/" + this.props.match.params.restaurantId)
+            .then(response => response.json())
+            .then(restaurant => this.setState({restaurant}));
+    }
+
+    getMeals(restaurantId) {
+        fetch("http://localhost:8080/catering_app_war_exploded/orders/active")
+            .then(response => response.json())
+            .then(orders => {
+                orders = orders.filter(order =>
+                    new Date(order.date).toDateString() === new Date().toDateString());
+
+                let meals = [];
+
+                for (let order of orders) {
+                    meals = meals.concat(order.meals.filter(meal => {
+                        return meal.restaurant.id === restaurantId;
+                    }));
+                }
+
+                console.log(meals);
+
+                this.setState({meals: meals});
+            });
+    }
+
+    componentDidMount() {
+        if (this.props.match.params.restaurantId) {
+            this.getRestaurantInfo();
+            this.getMeals(+this.props.match.params.restaurantId);
+        }
     }
 
     render() {
-        return (
-            <div className="restaurant">
+        if (this.state.restaurant) {
+            return (
+                <div className="restaurant">
+                    <h1>{this.state.restaurant.name}</h1>
+                    <h2>Zamówione posiłki na dzisiaj ({new Date().toDateString()}):</h2>
 
-            </div>
-        );
+                </div>
+            );
+        } else {
+            return <h1>"Ładuję restaurację..."</h1>;
+        }
     }
 }
 
